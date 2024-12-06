2f2f607013728772379472d1e0dcfb69e4310f28
Kasper Helsted <helsted@vopu.dk>
2024-12-06 13:28:53 +0100

    feat:Fixed deploy

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="certbot-dns-gigahost",
+    version="0.1.2",
+    description="Custom Certbot DNS Authenticator for MyDNSProvider",
+    packages=find_packages(),
+    install_requires=["certbot", "requests", "requests-mock", "bs4", "lxml", ],
+    entry_points={
+        "certbot.plugins": [
+            "dns-gigahost = certbot_dns_gigahost.authenticator:Authenticator",
+        ],
+    },
+)
